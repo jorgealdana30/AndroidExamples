e0f41f1b5e955200f9569cbe13549a851a556486
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2023-10-31 09:15:46 -0500

    Fixing issues with Android SDK 34

--- TextExample/app/build.gradle.kts
+++ TextExample/app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.jorgealdana.textexample"
         minSdk = 22
+        //noinspection EditedTargetSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
